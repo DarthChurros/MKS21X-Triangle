c7b608b0d8af1f894ea1923881c53157ccc3cc59
DarthChurros <tnarayan00@stuy.edu>
2018-10-10 20:36:57 -0400

    removed explicit call to toString

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver {
     public static void main(String[] args) {
     Triangle abc = new Triangle(0.0, 0.0, 3.0, 0.0, 0.0, 4.0);
 
-    System.out.println(abc.toString());
+    System.out.println(abc);
     System.out.println("\nPerimeter: " + abc.getPerimeter());
 
     System.out.println("\nVertices: " + abc.getVertex(0).toString() + ", " + abc.getVertex(1).toString() + ", " + abc.getVertex(1).toString());
